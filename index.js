4779e4d2295d453282ea6523254e12172f834144
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-16 03:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class BoardMember {
     return "You will see great things from Scuber."
   }
 
-  sayHi(){
+  sayHi = function (){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
